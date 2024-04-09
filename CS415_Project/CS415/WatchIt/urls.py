92502f2e8451015c88df9330ac37e93b2158d60e
karan0412 <karanparmarpcn@gmail.com>
2024-04-09 23:02:36 +1200

    Login, and logout working

--- CS415_Project/CS415/WatchIt/urls.py
+++ CS415_Project/CS415/WatchIt/urls.py
@@ -5,4 +5,8 @@ from .import views
 
 urlpatterns = [
     path("", views.Home, name="Home"),
+     path("SignUp/", views.SignUp, name="SignUp"),
+     path("Login/", views.Login, name="Login"),
+     path('Loggedin/', views.Loggedin, name='Loggedin'),
+     path("LogoutUser/", views.LogoutUser, name="LogoutUser"),
 ]
--- CS415_Project/CS415/WatchIt/views.py
+++ CS415_Project/CS415/WatchIt/views.py
@@ -1,5 +1,63 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def Home(request):
     return render(request, 'Home.html')
+
+
+def SignUp (request):
+    if request.method =='POST':
+        user_fname = request.POST.get('user_fname')
+        user_lname = request.POST.get('user_lname')
+        user_email = request.POST.get('user_email')
+        user_username = request.POST.get('user_username')
+        user_pwd = request.POST.get('user_pwd')
+
+        if User.objects.filter(username=user_username).exists():
+            messages.error(request, "Username already exists! Please Login.")
+        elif User.objects.filter(email=user_email).exists():
+            messages.error(request, "Email already exists! Please Login.")
+        else:
+            user = User.objects.create(first_name=user_fname, last_name=user_lname, email=user_email, username=user_username)
+            user.set_password(user_pwd) 
+            user.save()
+
+            return redirect("Login")
+            
+    return render(request, 'SignUp.html')
+
+def Login(request):
+    if request.method == 'POST':
+        username= request.POST.get('uname')
+        password = request.POST.get('pwd')
+        print({username})
+        print({password})
+        user = authenticate(request, username=username, password=password)
+        print({user})
+      
+            #user = User.objects.filter(user_email=user_email)
+            
+            #if user_pwd == user_pwd:    
+        if user is not None:     
+            login(request, user)    
+            return redirect('Loggedin')  # Redirect to dashboard view
+        else:
+            context = {'error': "Username or password did not match."}
+            return render(request, 'Login.html', context)
+    
+
+    return render(request, 'Login.html')
+
+@login_required
+def Loggedin(request):
+    
+    return render(request, 'LoggedIn.html')
+
+
+def LogoutUser(request):
+    logout(request)
+    return redirect('Login')
