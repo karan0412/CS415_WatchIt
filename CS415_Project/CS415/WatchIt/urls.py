e7a1e338db34c102e2a55af65d0f5b6af713b9b4
karan0412 <karanparmarpcn@gmail.com>
2024-06-01 21:48:32 +1200

    admin update

--- CS415_Project/CS415/WatchIt/urls.py
+++ CS415_Project/CS415/WatchIt/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path('movies/', views.movie_list, name='movie_list'),
     path('movies/<int:movie_id>/', views.movie_detail, name='movie_detail'),
     path('movies/<int:movie_id>/showtimes/', views.movie_showtimes, name='movie_showtimes'),
-    #path('book_seats/', views.book_seats, name='book_seats'),
     path('selectTickets/<int:cinema_hall_id>/<int:movie_id>/<int:showtime_id>/', views.selectTickets, name='selectTickets'),
     path('payment/<int:cinema_hall_id>/', views.payment, name='payment'),
     path('process-payment/', views.process_payment, name='process-payment'),
@@ -29,15 +28,12 @@ urlpatterns = [
     path('your_bookings/', views.your_bookings, name='your_bookings'),
     path('transaction_report/', views.transaction_report, name='transaction_report'),
     path('transaction_report/pdf/', views.transaction_report_pdf, name='transaction_report_pdf'),
-    
     path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
     path('edit_showtime/<int:booking_id>/<int:movie_id>/<int:cinema_hall_id>/', views.edit_showtime, name='edit_showtime'),
     path('edit_seats/<int:booking_id>/<int:showtime_id>/<int:cinema_hall_id>/', views.edit_seats, name='edit_seats'),
     path('confirm_edit_booking/<int:booking_id>/<int:showtime_id>/<str:seats>/', views.confirm_edit_booking, name='confirm_edit_booking'),
-    
     path('recommendations/', views.movie_recommendations, name='movie_recommendations'),
     path('generate_purchase_history/<int:booking_id>/', views.generate_purchase_history, name='generate_purchase_history'),
-
     path('activate-user/<uidb64>/<token>', views.activate_user, name='activate'),
     path('Login/forget_password/', views.forget_password, name='forget_password'),
     path('reset_password/<uidb64>/<token>/', views.reset_password, name='reset_password'),
@@ -45,29 +41,18 @@ urlpatterns = [
     path('enter_otp/', views.enter_otp, name='enter_otp'),
     path('resend-otp/', views.resend_otp, name='resend_otp'),
     path('QRcode', views.QRcode, name='QRcode'),
-
     path('generate_purchase_history/<int:booking_id>/', views.generate_purchase_history, name='generate_purchase_history'),
     path('send_sms/', views.send_test_sms, name='send_sms'),
     path('verify_otp_sms/', views.verify_otp_sms, name='verify_otp_sms'),
     path('Login_first/', views.Login_first, name='Login_first'),
     path('resend_otp_sms/', views.resend_otp_sms, name='resend_otp_sms'),
-    
-
-    # path('admin_tools/', include('admin_tools.urls')),
-    # path('admin/user_chart/', views.generate_user_chart, name='user_chart_view'),
     path('user_activity_report/', views.user_activity_report_view, name='user_activity_report'),
     path('view_log_entries/', views.view_log_entries, name='view_log_entries'),
     path('sales_report/', views.sales_report_view, name='sales_report'),
-
-
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('admin_dashboard/minute_sales/', views.minute_sales, name='admin_dashboard_minute_sales'),
     path('admin_dashboard/minute_registrations/', views.minute_registrations, name='admin_dashboard_minute_registrations'),
     path('submit_feedback/',views.submit_feedback, name='submit_feedback'),
-    # path('career_application/', views.career_application, name='career_application'),
-    # path('career_applications_list/', views.career_applications_list, name='career_applications_list'),
-    # path('approve_application/<int:application_id>/', views.approve_application, name='approve_application'),
-    # path('reject_application/<int:application_id>/', views.reject_application, name='reject_application'),
     path('feedback_list/', views.feedback_list, name='feedback_list'),
     path('approve_feedback/<int:feedback_id>/', views.approve_feedback, name='approve_feedback'),
     path('reject_feedback/<int:feedback_id>/', views.reject_feedback, name='reject_feedback'),
